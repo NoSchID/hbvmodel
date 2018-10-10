669c480ba3e2a95fbbe90b14f30a91cc3d33302d
Nora Schmit <n.schmit17@ic.ac.uk>
2018-10-10 17:54:30 +0100

    Changed progression from acute to chronic/immune compartment (a function of gamma_acute)

--- R/first_model.R
+++ R/first_model.R
@@ -9,7 +9,7 @@ require(deSolve)
 library(here)
 
 #### Data ----
-pop_size <- read.csv(here("testdata", "gambia_1985_pop.csv"))
+gambia_data <- read.csv(here("testdata", "edmunds_gambia_prev.csv"))
 
 
 #### Preparation of simulation parameters and notation ----
@@ -21,7 +21,7 @@ times <- seq(1890, 1890+runtime, stepsize) # Model simulates HBV epidemic from 1
 
 ## AGE GROUPS
 da <- 1                     # time spent in each age group = 1 year
-ages <- seq(1,80,da)       # Ages 1-80
+ages <- seq(0,80-da,da)     # Age at the beginning of each age group - max age reached is 80
 n_agecat <- length(ages)    # 100 age groups of 1 year
 
 ## DEFINITION OF INDICES
@@ -31,9 +31,9 @@ aindex <- (n_agecat+1):(2*n_agecat)       # Acute infection
 iindex <- (2*n_agecat+1):(3*n_agecat)     # Chronic infection
 rindex <- (3*n_agecat+1):(4*n_agecat)     # Immune
 # Age groupings
-childindex <- 1:5                            # Age groups 1-5 years
-juvindex <- 6:15                             # Age groups 6-15 years
-adultindex <- 16:80                         # Age groups 16-100 years
+childindex <- 1:which(ages == 5)                 # Age groups 0-5 years
+juvindex <- which(ages == 6):which(ages == 15)   # Age groups 6-15 years
+adultindex <- which(ages == 16):n_agecat         # Age groups 16-80 years
 
 
 #### Functions ----
@@ -54,11 +54,11 @@ hbv_model <- function(times, pop, parameters){
 
     # Differential equations
     dS <- - (diff(c(0,S))/da) - (foi * S) - (mu * S)
-    dA <- - (diff(c(0,A))/da) + (foi * S) - (p_chronic * A) - (gamma_acute * A) - (mu * A)
-    dI <- - (diff(c(0,I))/da) + (p_chronic * A) - (sag_loss * I) - (mu * I) - (mu_hbv * I)
-    dR <- - (diff(c(0,R))/da) + (gamma_acute * A) + (sag_loss * I) - (mu * R)
+    dA <- - (diff(c(0,A))/da) + (foi * S) - (p_chronic * gamma_acute * A) - ((1-p_chronic) * gamma_acute * A) - (mu * A)
+    dI <- - (diff(c(0,I))/da) + (p_chronic * gamma_acute * A) - (sag_loss * I) - (mu * I) - (mu_hbv * I)
+    dR <- - (diff(c(0,R))/da) + ((1-p_chronic) * gamma_acute * A) + (sag_loss * I) - (mu * R)
 
-    # Demography: restore additional deaths from last age group as births for constant pop
+    # Demography: restore additional deaths from last age group as births for constant population size
     dS[1] <- dS[1] + (b * N) + S[n_agecat]/da + A[n_agecat]/da + I[n_agecat]/da + R[n_agecat]/da
     # all babies are born susceptible
 
@@ -91,11 +91,11 @@ run_model <- function(init_pop, times, parameters) {
 ## DEMOGRAPHY
 # Initial population
 init_pop <- c(
-  S = c(rep(20400,5), rep(5050,10), rep(1350,30), rep(186,35)),
-  A = c(rep(600,5), rep(200,10), rep(50,30), rep(14,35)),
-  I = c(rep(5950,5), rep(4375,10), rep(1400,30), rep(160,35)),
-  R = c(rep(8050,5), rep(7875,10), rep(6533,30), rep(1640,35))
-  )
+  S = c(rep(17000,6), rep(5050,10), rep(1350,30), rep(186,34)),
+  A = c(rep(500,6), rep(200,10), rep(50,30), rep(14,34)),
+  I = c(rep(4959,6), rep(4375,10), rep(1400,30), rep(160,34)),
+  R = c(rep(6708,6), rep(7875,10), rep(6533,30), rep(1640,34))
+)
 
 #init_pop <- c(
 #  S = c(rep(20400,5), rep(5050,10), rep(1350,30), rep(186,35)),
@@ -110,15 +110,13 @@ mu <- 0.014   # background mortality rate, assumed based on 70 years life expect
 b <- mu       # birth rate is assumed to equal the mortality rate
 
 ## TRANSMISSION
-b1 <- 2     # beta-child (up to 5-year olds)
-b2 <- 0.5   # beta-young (up to 15-year olds)
-b3 <- 0.5   # beta-all (over 5-year olds)
-# leads to very quick depletion of susceptibles but high prevalence (nearly 10%)
-# alternative: 1,0.1,0.1 maintains susceptible pool but only 5% prevalence
+b1 <- 0.4     # beta-child (up to 5-year olds)
+b2 <- 0.05   # beta-young (up to 15-year olds)
+b3 <- 0.015   # beta-all (over 5-year olds)
 
 # WAIFW matrix
 # Assuming no effective contact between children and adults
-beta <- matrix(0, nrow=n_agecat,ncol=n_agecat)     # matrix of transmission rates
+beta <- matrix(0, nrow = n_agecat, ncol = n_agecat)# matrix of transmission rates
 beta[childindex,childindex] <- b1                  # transmission among children (1-5 years)
 beta[juvindex,juvindex] <- b2                      # transmission among juveniles (6-15 years)
 beta[adultindex,adultindex] <- b3                  # transmission among adults (16-100 years)
@@ -134,12 +132,11 @@ beta[adultindex, juvindex] <- b3                   # transmission from adults to
 # rate unit is annual so each time in the model is 1 year
 parameters <- c(gamma_acute = 4, sag_loss = 0.01,
                 alpha = 0.16, mu = mu, mu_hbv = 0, b = b) # mu_hbv =0.0003
+# gamma_acute changed manually to fit prevalence, was 4 in Edmunds
 
 # Age-dependent probability of becoming a chronic carrier:
-# 0.9 in first group (corresponding to 0 years?)
-# 0.4 until age 5 years
-# 0.05 for everyone older
-p_chronic <- c(0.9, rep(0.4, max(childindex)-1), rep(0.05, n_agecat-max(childindex)))
+# Edmunds approach (except 0.89 for whole first year instead of just 0.5 years):
+p_chronic <- c(0.89, exp(-0.65*ages[-1]^0.46))
 
 #### Output ----
 out <- run_model(init_pop, times, parameters)
@@ -150,10 +147,11 @@ sus <- out[,(1+sindex)]
 acute <- out[,(1+aindex)]
 carriers <- out[,(1+iindex)]
 immune <- out[,(1+rindex)]
+ever_infected <- acute + carriers + immune
 totalpop <- apply(out[,-1], 1, sum)
 agespec_pop <- sus + acute + carriers + immune
 
-## Visualisation of population dynamics/aging
+## Visualisation of population dynamics and aging
 # Evolution of aging in 3 broad groups (children, juveniles, adults)
 all_children <- (apply(carriers[,childindex],1,sum)
                  + apply(sus[,childindex],1,sum)
@@ -172,7 +170,7 @@ lines(time, all_juveniles, col = "red")
 lines(time, all_adults, col = "blue")
 
 # Age distribution after equilibrium is reached (takes long to stabilise)
-plot(1:80, agespec_pop[2000,])
+plot(1:n_agecat, agespec_pop[2000,])
 
 ## Visualisation of infection dynamics
 # Susceptibles, infectious and immune (all age groups added)
@@ -182,15 +180,18 @@ lines(time,apply(sus,1,sum),col= "red")
 lines(time,apply(immune,1,sum),col= "blue")
 
 # Chronic prevalence by age group over time
-plot(time, apply(carriers[,childindex],1,sum), ylim = c(0,N0))
+plot(time, apply(carriers[,childindex],1,sum), type = "l", ylim = c(0,N0))
 lines(time, apply(carriers[,juvindex],1,sum), col = "red")
 lines(time, apply(carriers[,adultindex],1,sum), col = "blue")
 
 # Prevalence by age at 1 time point (after equilibrium is reached)
-plot(1:n_agecat, carriers[2000,]/agespec_pop[2000,], ylim = c(0,0.2))
+plot(1:n_agecat, carriers[2000,]/agespec_pop[2000,], type = "l", ylim = c(0,0.3))
+# Add Gambia data
+points(gambia_data$age, gambia_data$edmunds_prev)
 
-# Proportion immune at 1 time point (after equilibrium is reached)
-plot(1:n_agecat, immune[2000,]/agespec_pop[2000,], ylim = c(0,1))
+# Proportion ever infected at 1 time point (after equilibrium is reached)
+plot(1:n_agecat, ever_infected[2000,]/agespec_pop[2000,], type = "l", ylim = c(0,1))
+points(gambia_data$age, gambia_data$edmunds_prop_ever_infected)
 
 ### Run model checks
 # devtools::test()
