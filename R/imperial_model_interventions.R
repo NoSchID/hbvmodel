2740802159241fa403e107859c2b63bf9b36451a
Nora Schmit <n.schmit17@ic.ac.uk>
2020-03-04 18:36:23 +0000

    Added apply_screen_not_treat parameter to be able to move population to screening/monitoring compartments without treatment - this allows to follow a cohort in the status quo scenario

--- R/imperial_model_interventions.R
+++ R/imperial_model_interventions.R
@@ -842,22 +842,116 @@ reset_pop_1950 <- function(timestep, pop, parameters){
 screen_pop <- function(timestep, pop, parameters){
   with (as.list(pop),{
 
-    # Define indices
-    #nat_hist_f <- 1:(n_nathistcat*n_agecat)
-    #screened_f <- (max(nat_hist_f)+1):((n_nathistcat+n_screencat)*n_agecat)
-    #treated_f <- (max(screened_f)+1):(n_infectioncat*n_agecat)
-    #nat_hist_m <- (max(treated_f)+1):((n_infectioncat+n_nathistcat)*n_agecat)
-    #screened_m <- (max(nat_hist_m)+1):((n_infectioncat+n_nathistcat+n_screencat)*n_agecat)
-    #treated_m <- (max(screened_m)+1):(2*n_infectioncat*n_agecat)
+    # Define indices for age groupts to screen
+    age_groups_to_screen <- which(ages==parameters$min_age_to_screen):which(ages==parameters$max_age_to_screen)
 
-    # Select the compartments to screen: infection compartments 1-9 (S-R), women and men
-    #comps_to_screen <- array(unlist(pop[c(nat_hist_f, nat_hist_m)]),
-    #                         dim=c(n_agecat,n_nathistcat,2))
-    # Select the screened compartments: infection compartments 10-18 (S_S-R_S)
-    #comps_screened <- array(unlist(pop[c(screened_f, screened_m)]),
-    #                      dim=c(n_agecat,n_screencat,2))
-    #comps_treated <- array(unlist(pop[c(treated_f, treated_m)]),
-    #                       dim=c(n_agecat,n_treatcat,2))
+    # Select state variables array
+    total_pop <- array(unlist(pop[1:(2 * n_infectioncat * n_agecat)]),dim=c(n_agecat,n_infectioncat,2))
+
+    # Record the total number of people screened at each screening event
+    # Note this is not a cumulative output in the model and stays the same for years where there
+    # is no screening programme (sum unique values to calculate total HBsAg tests)
+    pop_to_screen_susceptible <- parameters$screening_coverage * total_pop[age_groups_to_screen,1,1:2]
+    pop_to_screen_immune <- parameters$screening_coverage * total_pop[age_groups_to_screen,9,1:2]
+    pop_to_screen_it <- parameters$screening_coverage * total_pop[age_groups_to_screen,2,1:2]
+    pop_to_screen_chb <- parameters$screening_coverage * total_pop[age_groups_to_screen,c(3,5),1:2]
+    pop_to_screen_cirrhosis <- parameters$screening_coverage * total_pop[age_groups_to_screen,c(6,7),1:2]
+    pop_to_screen_ineligible <- parameters$screening_coverage * total_pop[age_groups_to_screen,c(4,8),1:2]
+
+    total_screened_susceptible <- sum(pop_to_screen_susceptible)
+    total_screened_immune <- sum(pop_to_screen_immune)
+    total_screened_it <- sum(pop_to_screen_it)
+    total_screened_chb <- sum(pop_to_screen_chb)
+    total_screened_cirrhosis <- sum(pop_to_screen_cirrhosis)
+    total_screened_ineligible <- sum(pop_to_screen_ineligible)
+
+    # Calculate the population to screen then treat in the treatment eligible compartments:
+    # IT (2) - optional, IR (3), ENCHB (5), CC (6), DCC (7)
+    pop_to_treat_it <- parameters$screening_coverage * parameters$link_to_care_prob *
+      parameters$treatment_initiation_prob_it[age_groups_to_screen]  * total_pop[age_groups_to_screen,2,1:2]
+    # Only >30 year old IT people can be treated
+
+    pop_to_treat_ir <- parameters$screening_coverage * parameters$link_to_care_prob *
+      parameters$treatment_initiation_prob * total_pop[age_groups_to_screen,3,1:2]
+
+    pop_to_treat_enchb <- parameters$screening_coverage * parameters$link_to_care_prob *
+      parameters$treatment_initiation_prob * total_pop[age_groups_to_screen,5,1:2]
+
+    pop_to_treat_cc <- parameters$screening_coverage * parameters$link_to_care_prob *
+      parameters$treatment_initiation_prob * total_pop[age_groups_to_screen,6,1:2]
+
+    pop_to_treat_dcc <- parameters$screening_coverage * parameters$link_to_care_prob *
+      parameters$treatment_initiation_prob * total_pop[age_groups_to_screen,7,1:2]
+
+    # Calculate the population identified as treatment-ineligible (eligible for monitoring):
+    # IT (2) - if not treated, IC (4)
+    pop_to_monitor_it <- parameters$screening_coverage * parameters$link_to_care_prob *
+      total_pop[age_groups_to_screen,2,1:2] - pop_to_treat_it
+
+    pop_to_monitor_ic <- parameters$screening_coverage * parameters$link_to_care_prob *
+      total_pop[age_groups_to_screen,4,1:2]
+
+    # Move treated/screened IT (now 2 monitor comps)
+
+    # Move screened susceptibles to post-screening immune compartment (vaccination): V_S (10)
+    # This assumes there is no re-testing unless prop_to_vaccinate is not 1
+    total_pop[age_groups_to_screen,1,1:2] <- total_pop[age_groups_to_screen,1,1:2] -
+      parameters$prop_to_vaccinate * pop_to_screen_susceptible
+
+    total_pop[age_groups_to_screen,10,1:2] <- total_pop[age_groups_to_screen,10,1:2] +
+      parameters$prop_to_vaccinate * pop_to_screen_susceptible
+
+    # Remove the population to treat from undiagnosed compartments
+    total_pop[age_groups_to_screen,2,1:2] <- total_pop[age_groups_to_screen,2,1:2] -
+      pop_to_treat_it
+    total_pop[age_groups_to_screen,3,1:2] <- total_pop[age_groups_to_screen,3,1:2] -
+      pop_to_treat_ir
+    total_pop[age_groups_to_screen,5,1:2] <- total_pop[age_groups_to_screen,5,1:2] -
+      pop_to_treat_enchb
+    total_pop[age_groups_to_screen,6,1:2] <- total_pop[age_groups_to_screen,6,1:2] -
+      pop_to_treat_cc
+    total_pop[age_groups_to_screen,7,1:2] <- total_pop[age_groups_to_screen,7,1:2] -
+      pop_to_treat_dcc
+
+    # Move the population from undiagnosed to treatment-ineligible (to monitor) compartments
+    # IT_S (11) and IC_S (13)
+    total_pop[age_groups_to_screen,2,1:2] <- total_pop[age_groups_to_screen,2,1:2] - pop_to_monitor_it
+    total_pop[age_groups_to_screen,11,1:2] <- total_pop[age_groups_to_screen,11,1:2] + pop_to_monitor_it
+
+    total_pop[age_groups_to_screen,4,1:2] <- total_pop[age_groups_to_screen,4,1:2] - pop_to_monitor_ic
+    total_pop[age_groups_to_screen,13,1:2] <- total_pop[age_groups_to_screen,13,1:2] + pop_to_monitor_ic
+
+    if (parameters$apply_screen_not_treat == 0) {
+      # Add the population to treat to treated compartments
+      # IT_T (19), CHB_T (20), CC_T (21), DCC_T (22)
+
+      total_pop[age_groups_to_screen,19,1:2] <- total_pop[age_groups_to_screen,19,1:2] + pop_to_treat_it
+      total_pop[age_groups_to_screen,20,1:2] <- total_pop[age_groups_to_screen,20,1:2] + pop_to_treat_ir +
+        pop_to_treat_enchb
+      total_pop[age_groups_to_screen,21,1:2] <- total_pop[age_groups_to_screen,21,1:2] + pop_to_treat_cc
+      total_pop[age_groups_to_screen,22,1:2] <- total_pop[age_groups_to_screen,22,1:2] + pop_to_treat_dcc
+
+    } else if (parameters$apply_screen_not_treat == 1) {
+      # If we just want to follow a screened but untreated cohort, move pop_to_treat to
+      # monitoring compartments instead
+
+      total_pop[age_groups_to_screen,11,1:2] <- total_pop[age_groups_to_screen,11,1:2] + pop_to_treat_it
+      total_pop[age_groups_to_screen,12,1:2] <- total_pop[age_groups_to_screen,12,1:2] + pop_to_treat_ir
+      total_pop[age_groups_to_screen,14,1:2] <- total_pop[age_groups_to_screen,14,1:2] + pop_to_treat_enchb
+      total_pop[age_groups_to_screen,15,1:2] <- total_pop[age_groups_to_screen,15,1:2] + pop_to_treat_cc
+      total_pop[age_groups_to_screen,16,1:2] <- total_pop[age_groups_to_screen,16,1:2] + pop_to_treat_dcc
+
+      }
+
+
+    return(c(total_pop, unlist(init_pop[(2*n_infectioncat*n_agecat+1):(length(init_pop)-6)]),
+             total_screened_susceptible, total_screened_immune, total_screened_it, total_screened_chb,
+             total_screened_cirrhosis, total_screened_ineligible))
+  })
+}
+
+screen_pop_before_changes <- function(timestep, pop, parameters){
+  with (as.list(pop),{
 
     # Define indices for age groupts to screen
     age_groups_to_screen <- which(ages==parameters$min_age_to_screen):which(ages==parameters$max_age_to_screen)
@@ -881,6 +975,7 @@ screen_pop <- function(timestep, pop, parameters){
     total_screened_chb <- sum(pop_to_screen_chb)
     total_screened_cirrhosis <- sum(pop_to_screen_cirrhosis)
     total_screened_ineligible <- sum(pop_to_screen_ineligible)
+
 
     # Calculate the population to screen then treat in the treatment eligible compartments:
     # IT (2) - optional, IR (3), ENCHB (5), CC (6), DCC (7)
@@ -2533,6 +2628,62 @@ run_one_scenario <- function(..., default_parameter_list, calibrated_parameter_s
   return(outlist)
 }
 
+# Scenario can be specified
+run_one_scenario_parallel <- function(..., default_parameter_list, calibrated_parameter_sets,
+                             parms_to_change = list(...),
+                             drop_timesteps_before = NULL,
+                             scenario) {
+
+  # Status quo scenario: no screening
+  sim <- parApply(cl = cl, calibrated_parameter_sets, 1,
+               function(x) run_model(sim_duration = runtime,
+                                     default_parameter_list = default_parameter_list,
+                                     parms_to_change =
+                                       list(b1 = as.list(x)$b1,
+                                            b2 = as.list(x)$b2,
+                                            b3 = as.list(x)$b3,
+                                            mtct_prob_s = as.list(x)$mtct_prob_s,
+                                            mtct_prob_e = as.list(x)$mtct_prob_e,
+                                            alpha = as.list(x)$alpha,
+                                            p_chronic_in_mtct = as.list(x)$p_chronic_in_mtct,
+                                            p_chronic_function_r = as.list(x)$p_chronic_function_r,
+                                            p_chronic_function_s = as.list(x)$p_chronic_function_s,
+                                            pr_it_ir = as.list(x)$pr_it_ir,
+                                            pr_ir_ic = as.list(x)$pr_ir_ic,
+                                            eag_prog_function_rate = as.list(x)$eag_prog_function_rate,
+                                            pr_ir_enchb = as.list(x)$pr_ir_enchb,
+                                            pr_ir_cc_female = as.list(x)$pr_ir_cc_female,
+                                            pr_ir_cc_age_threshold = as.list(x)$pr_ir_cc_age_threshold,
+                                            pr_ic_enchb = as.list(x)$pr_ic_enchb,
+                                            sag_loss_slope = as.list(x)$sag_loss_slope,
+                                            pr_enchb_cc_female = as.list(x)$pr_enchb_cc_female,
+                                            cirrhosis_male_cofactor = as.list(x)$cirrhosis_male_cofactor,
+                                            pr_cc_dcc = as.list(x)$pr_cc_dcc,
+                                            cancer_prog_coefficient_female = as.list(x)$cancer_prog_coefficient_female,
+                                            cancer_age_threshold = as.list(x)$cancer_age_threshold,
+                                            cancer_male_cofactor = as.list(x)$cancer_male_cofactor,
+                                            hccr_it = as.list(x)$hccr_it,
+                                            hccr_ir = as.list(x)$hccr_ir,
+                                            hccr_enchb = as.list(x)$hccr_enchb,
+                                            hccr_cc = as.list(x)$hccr_cc,
+                                            hccr_dcc = as.list(x)$hccr_dcc,
+                                            mu_cc = as.list(x)$mu_cc,
+                                            mu_dcc = as.list(x)$mu_dcc,
+                                            mu_hcc = as.list(x)$mu_hcc,
+                                            vacc_eff = as.list(x)$vacc_eff),
+                                     drop_timesteps_before = drop_timesteps_before,
+                                     scenario = scenario))
+  out <- lapply(sim, code_model_output)
+
+  label <- paste0("out_", scenario)
+
+  outlist <- list("out" = out)
+  names(outlist) <- label
+
+  return(outlist)
+}
+
+
 ### MODEL INPUT ----
 
 # DEMOGRAPHY: SEE ABOVE
@@ -2719,6 +2870,7 @@ parameter_list <- list(
   apply_vacc = 1,
   apply_bdvacc = 0,
   apply_treat_it = 0,                        # Switch for IT >30 year olds being eligible for treatment (default = off)
+  apply_screen_not_treat = 0,                # Switch to only screen+assess but not treat (allows to follow an untreated cohort)
   # DEMOGRAPHY ON/OFF SWITCH (1/0)
   births_on = 1,
   migration_on = 1,
