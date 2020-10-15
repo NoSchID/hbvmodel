b47a6d3f62134e7b668bcda71d99c5b327daff84
Nora Schmit <n.schmit17@ic.ac.uk>
2020-10-15 13:06:08 +0100

    Added parameter sets A2 and A3 for screening different age groups

--- R/scenario_analysis/screen_and_treat_strategies_simulations.R
+++ R/scenario_analysis/screen_and_treat_strategies_simulations.R
@@ -12,7 +12,7 @@ source(here("R/scenario_analysis/calculate_outcomes.R"))
 
 # UPDATED SCENARIOS 22/09/20
 
-# Age group scenarios: 15-60 (A1), 15-30 (A2), 30-45 (A3), 30-60 (A4)
+# Age group scenarios: 15-60 (A1), 45-60 (A2), 30-60 (A3), 15-30 (A4), 30-45 (A5)
 
 # ASSUMPTION A1 SIMULATIONS
 scenario_a1_parms <- parameter_list
@@ -26,6 +26,33 @@ scenario_a1_parms$monitoring_prob <- 0.8
 scenario_a1_parms$apply_bdvacc <- 0
 scenario_a1_parms$apply_treat_it <- 0
 #save(scenario_a1_parms, file= here("analysis_input", "scenario_a1_parms.Rdata"))
+
+# ASSUMPTION A2 SIMULATIONS
+scenario_a2_parms <- parameter_list
+scenario_a2_parms$screening_coverage <- 0.9
+scenario_a2_parms$min_age_to_screen <- 45
+scenario_a2_parms$max_age_to_screen <- 60
+scenario_a2_parms$prop_to_vaccinate <- 0
+scenario_a2_parms$link_to_care_prob <- 0.8
+scenario_a2_parms$treatment_initiation_prob <- 1
+scenario_a2_parms$monitoring_prob <- 0.8
+scenario_a2_parms$apply_bdvacc <- 0
+scenario_a2_parms$apply_treat_it <- 0
+#save(scenario_a2_parms, file= here("analysis_input", "scenario_a2_parms.Rdata"))
+
+# ASSUMPTION A3 SIMULATIONS
+scenario_a3_parms <- parameter_list
+scenario_a3_parms$screening_coverage <- 0.9
+scenario_a3_parms$min_age_to_screen <- 30
+scenario_a3_parms$max_age_to_screen <- 60
+scenario_a3_parms$prop_to_vaccinate <- 0
+scenario_a3_parms$link_to_care_prob <- 0.8
+scenario_a3_parms$treatment_initiation_prob <- 1
+scenario_a3_parms$monitoring_prob <- 0.8
+scenario_a3_parms$apply_bdvacc <- 0
+scenario_a3_parms$apply_treat_it <- 0
+#save(scenario_a3_parms, file= here("analysis_input", "scenario_a3_parms.Rdata"))
+
 
 # INITIAL SCENARIOS (before 22/09/20) ----
 
